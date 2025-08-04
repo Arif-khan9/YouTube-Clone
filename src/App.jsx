0ba0ce5c03711fd084eb65b7c75259991916cd39
Arif-khan9 <ak4040651@gmail.com>
2025-08-04 14:36:51 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Routes  } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+
+
+const App = () => {
+  
+  return (
+    <div>
+     <Navbar/>
+     <Home/>
+    {/* <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/video/categoryId/:videoId' element={<Video/>}/>
+    </Routes> */}
+    </div>
+  )
+}
+
+export default App
